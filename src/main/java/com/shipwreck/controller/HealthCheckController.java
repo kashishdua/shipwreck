7107c0761eb62c43cc5f76f67abdf43ade464fea
Kashish Dua <kashish.dua@nyu.edu>
2018-07-20 02:43:09 -0400

    Rest Controllers created. Rest endpoints not created yet. UI setup, not connected to rest endpoints.

---
+++ src/main/java/com/shipwreck/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.shipwreck.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @RequestMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
